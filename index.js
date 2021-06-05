82426b4a0281bc919ca8e8785e02c187f231e6a5
tsn_nagarajan <36725099+Nagarajan-ThammannaSrinivasan@users.noreply.github.com>
2021-06-05 15:01:08 +0530

    Update index.js
    setInterval with ws.Ping

--- index.js
+++ index.js
@@ -1,122 +1,135 @@
-// var server = require('ws').Server;
-// var PORT =  5000;
-// var s = new server({port: PORT});
-// // process.env.PORT ||
-// var express = require('express')
-// var app = express();
-// app.use(express.urlencoded({
-//     extended: true
-//   }));
+const { timeStamp } = require('console')
+const express = require('express')
+const http = require('http')
+const { stringify } = require('querystring')
+const WebSocket = require('ws')
 
-// app.use(express.json())
+const port = process.env.PORT || 5000
+console.log(port)
+const app = express()
+const httpServer = http.createServer(app)
+const wss = new WebSocket.Server({
+    'server': httpServer
+})
+httpServer.listen(port)
 
-// var nodeHttpserver = app.listen(PORT, () =>{
-//     var host = nodeHttpserver.address().address
-//     var port = nodeHttpserver.address().port
-//     console.log("Example app listening at http://%s:%s", host, port)
-//     console.log('up and running')
-// })
-
-
-// s.on('connection', function(ws){
-//     ws.emit('An event emiited')
-//     ws.on('message', function(msg){
-//         console.log('msg received is ' + msg );
-     
-//     })
-
-   
-//     var gpsData = {
-//         id: '1',
-//         lat: 80.0,
-//         long: 23.4 
-//     }
-//     ws.send(JSON.stringify(gpsData))
-
-//     // sendDataFromWS(ws)
-
-//     setTimeout( function(){
-//         ws.send('Greetings from Server')
-//     }    , 10000)
-// })
-
-// // var oracleDB = require('oracledb')
-
-
-
-// app.post('/postLocation',  (req, res) =>{
-//     console.log(req.body)
-//     // ws.send(JSON.stringify(req))
-//     // fs.readFile( __dirname + "/" + "truckLocation.json", 'utf8', function (err, data) {
-//     //    console.log( data );
-//     //    res.end( data );
-//     // });
-//     res.end(JSON.stringify(req.body))
-//     s.clients.forEach(client =>{        
-//         client.send(JSON.stringify(req.body))
-//     })
-//  })
-
- /////
- const express = require('express')
- const http = require('http')
- const WebSocket = require('ws')
- 
- const port = process.env.PORT || 5000
- const app = express()
- const httpServer = http.createServer(app)
- const wss = new WebSocket.Server({
-     'server': httpServer
- })
- httpServer.listen(port)
-
- app.use(express.urlencoded({
-    extended: true
-  }));
+app.use(express.urlencoded({
+    extended: true  
+}));
 
 app.use(express.json())
 
- wss.on("connection", function(ws){
-    console.log('connected')
-    ws.on('message', function(msg){
-         ws.send('__Pong__')
-        console.log('msg received is ' + msg );         
-    })
-    setTimeout( function(){
-        ws.send('Greetings from Server')
-    }, 10000)
- })
-
- wss.on('close', function(ws){
-     console.log(" wss close");
-     console.log(ws)
- })
-
- wss.on('error', function(ws){
-    console.log(" wss error");
-    console.log(ws)
+app.use(function(req, res, next) {
+    if (req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization')
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE')
+        if (req.method === 'OPTIONS') return res.send(200)
+    }
+    next()
 })
 
+var connectedClients = [];
+
+var standardPayload = {
+    platform : "Browser",
+    msgType  : "newClient",
+    data : {}
+}
+
+ wss.on("connection", function(ws, req){
+    // Create Id for each connected Client
+    ws.id = Date.now() + Math.random();
+    // Push all clients into a list
+    connectedClients.push(ws);
+    
+    ws.on('ping', function(data){
+        console.log('onping');
+        console.log(data)
+    })
+
+    ws.on('pong', function(data){
+        console.log('onpong');
+        console.log(data)
+    })
+    // connectedClients.forEach(x =>{
+    //     console.log(x.id )
+    // })
+
+    // When Server gets a message from clients
+    ws.on('message', function(msg){ 
+        msg = JSON.parse(msg); 
+        ws.platform = msg.platform == "Browser" ? "Browser" : "Mobile";
+        console.log(ws.platform)
+        // When client gets connected, send back their Id
+        standardPayload ={
+            platform : ws.platform,
+            msgType : "ClientID",
+            data:{
+                ID : ws.id
+            }
+        }
+        ws.send(JSON.stringify(standardPayload));
+
+        // var cur_Time = new Date();
+        // console.log(cur_Time.getHours() + ':'+ cur_Time.getMinutes() + ":" + cur_Time.getSeconds() + ":"  + msg );         
+    })
+
+    // When client gets closed, pop them from clients list
+    ws.on('close', function(){
+        console.log(this.id + " closed");
+        var indexClientID = connectedClients.indexOf(this);
+        // console.log(indexClientID);
+        connectedClients.splice(indexClientID, 1);
+        // console.log("length " + connectedClients.length);
+        // connectedClients.forEach(x =>{
+        //     console.log(x.id )
+        // })
+    })
+   
+   // When client gets errored
+    ws.on('error', function(){
+       console.log(this.id + " errored");
+       var indexClientID = connectedClients.indexOf(this);
+       // console.log(indexClientID);
+       connectedClients.splice(indexClientID, 1);
+   })
+
+   setInterval(() => {
+       ws.ping('Ping')
+   }, 10000);
+ })
+
+
+
  app.get('/',  (req, res) =>{
-     res.send("Hello Buddy !!")
+    console.log("hit")
+    res.send("Hello Buddy !!")
  })
 
  app.post('/postLocation',  (req, res) =>{
-    console.log(req.body)
+    // console.log(req.body)
     // ws.send(JSON.stringify(req))
     // fs.readFile( __dirname + "/" + "truckLocation.json", 'utf8', function (err, data) {
     //    console.log( data );
     //    res.end( data );
     // });
     res.send(req.body)
-//     res.end(JSON.stringify(req.body))
-    console.log(wss.clients[0])
-   wss.clients.forEach(client =>{      
+    var incomingPayload = req.body.standardPayload;
+    
+    wss.clients.forEach(client =>{   
+        if(client.id == incomingPayload.data.clientID){
+            client.geolocation = {
+                lat : incomingPayload.data.lat,
+                long : incomingPayload.data.long
+            }
+            // console.log(client.geolocation)
+        } 
         if(client.OPEN == 1  ){
-            client.send(JSON.stringify(req.body))
+            client.send(JSON.stringify(incomingPayload))
         }
         else{
-            console.log("CLOSED")
+            console.log(client.id + " CLOSED")
         }
     })
 })
